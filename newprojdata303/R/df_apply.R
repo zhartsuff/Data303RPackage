0617879576e7abfe47928849cd2900867d87f9e6
Grace Van Farowe <92184317+garcie@users.noreply.github.com>
2023-03-17 16:00:52 -0400

    Update df_apply.R

--- newprojdata303/R/df_apply.R
+++ newprojdata303/R/df_apply.R
@@ -22,30 +22,3 @@ df_apply <- function(.data, .fun, .filter, ...) {
 #'try_iris <- df_apply(iris, round, is.numeric)
 #'> try_iris
 #'where does it go?
-
-
-iris_rounded <- iris
-iris_rounded$Sepal.Length <- round(iris$Sepal.Length)
-iris_rounded$Sepal.Width <- round(iris$Sepal.Width)
-iris_rounded$Petal.Length <- round(iris$Petal.Length)
-iris_rounded$Petal.Width <- round(iris$Petal.Width)
-
-
-
-simple_df <- cbind(
-  c("A", "B", "C", "D", "E"),
-  c("the", "cat", "in", "the", "hat"),
-  c(3.4758, 4.2, 5.3248, 435.54326, 9999.999999),
-  c(4, 6, 23, 54, 12),
-  c("Gabby", "Zander", "Grace", "Somebody", "Randall")
-)
-simple_df <- data.frame(simple_df)
-
-simple_df$X3 <- as.numeric(simple_df$X3)
-simple_df$X4 <- as.numeric(simple_df$X4)
-
-simple_up <- simple_df
-simple_up$X1 <- toupper(simple_up$X1)
-simple_up$X2 <- toupper(simple_up$X2)
-simple_up$X5 <- toupper(simple_up$X5)
-
