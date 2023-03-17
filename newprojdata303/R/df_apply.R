86c2ae17503dc97a252c4abafb3bb409af513d93
gef4 <122303623+gef4@users.noreply.github.com>
2023-03-17 15:52:20 -0400

    backup

--- newprojdata303/R/df_apply.R
+++ newprojdata303/R/df_apply.R
@@ -29,4 +29,23 @@ iris_rounded$Sepal.Length <- round(iris$Sepal.Length)
 iris_rounded$Sepal.Width <- round(iris$Sepal.Width)
 iris_rounded$Petal.Length <- round(iris$Petal.Length)
 iris_rounded$Petal.Width <- round(iris$Petal.Width)
-iris_rounded
+
+
+
+simple_df <- cbind(
+  c("A", "B", "C", "D", "E"),
+  c("the", "cat", "in", "the", "hat"),
+  c(3.4758, 4.2, 5.3248, 435.54326, 9999.999999),
+  c(4, 6, 23, 54, 12),
+  c("Gabby", "Zander", "Grace", "Somebody", "Randall")
+)
+simple_df <- data.frame(simple_df)
+
+simple_df$X3 <- as.numeric(simple_df$X3)
+simple_df$X4 <- as.numeric(simple_df$X4)
+
+simple_up <- simple_df
+simple_up$X1 <- toupper(simple_up$X1)
+simple_up$X2 <- toupper(simple_up$X2)
+simple_up$X5 <- toupper(simple_up$X5)
+
