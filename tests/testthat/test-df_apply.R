bf8b9e0b566c2ff204aef61d2c12be0aa2c1c843
garcie <you@example.com>
2023-03-17 16:45:18 -0400

    test

--- tests/testthat/test-df_apply.R
+++ tests/testthat/test-df_apply.R
@@ -1,8 +1,9 @@
-test_that("", {
+test_that("A manually created rounded iris data set is equal to what our function creates (see data folder)", {
   expect_equal(df_apply(iris, round, is.numeric), iris_rounded)
 })
 
-
-test_that("", {
+test_that("Our function properly turns character vectors in a data frame to upper case", {
   expect_equal(df_apply(simple_df, toupper, is.character), simple_up)
 })
+
+
