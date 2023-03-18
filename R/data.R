f8ecf82401e5897316a16b4f99543e34e1a31bbf
garcie <you@example.com>
2023-03-18 13:35:47 -0400

    everything is working

---
+++ R/data.R
@@ -0,0 +1,61 @@
+#' Testing data for data 303 project
+#'
+#' Simple data containing columns of numeric and character data for use in testing
+#' df_apply function. 
+#'
+#' @format ## `simple_df`
+#' A data frame with 5 rows and 5 columns
+#' \describe{
+#'   \item{X1}{A vector of the letters A-E}
+#'   \item{X2}{The words the, cat, in, the, hat}
+#'   \item{X3}{Random numbers in decimal format}
+#'   \item{X4}{Random integers}
+#'   \item{X5}{Names with upper and lower case letters}
+#' @source <No source>
+"simple_df"
+
+#' Testing data for data 303 project
+#'
+#' Simple data containing columns of numeric and character data for use in testing
+#' df_apply function, all in uppercase
+#'
+#' @format ## `simple_up`
+#' A data frame with 5 rows and 5 columns
+#' \describe{
+#'   \item{X1}{A vector of the letters A-E}
+#'   \item{X2}{The words THE, CAT, IN, THE, HAT}
+#'   \item{X3}{Random numbers in decimal format}
+#'   \item{X4}{Random integers}
+#'   \item{X5}{Names with upper case letters}
+#' @source <No source>
+"simple_up"
+
+#' Climate conditions of the boloria chariclea butterfly
+#'
+#' Data from a study of wing lengths of butterflies
+#'
+#' @format ## `boloria`
+#' A data frame with 5 rows and 5 columns
+#' \describe{
+#'   \item{year}{Data collected from 1996 - 2013}
+#'   \item{site}{Seven sites where measurements were taken}
+#'   \item{sex}{sex of butterfly}
+#'   \item{WL}{Wing length}
+#'   \item{DOY}{Day of year}
+#'   \item{Snow}{Snowfall in year in inches}
+#'   \item{mayjun}{Diffence in wing length from May to June}
+#' @source <https://datadryad.org/stash/dataset/doi:10.5061/dryad.43gt3>
+"boloria"
+
+#' Iris data set rounded
+#'
+#' Base R iris data set with numeric columns rounded
+#'
+#' @format ## `iris_rounded`
+#' iris_rounded is a data frame with 150 cases (rows) and 5 variables (columns) 
+#' named Sepal.Length, Sepal.Width, Petal.Length, Petal.Width, and Species.
+#' \describe{
+#'   \item{all}{Use help(iris) to see details about data}
+#
+#' @source <iris package in base r>
+"iris_rounded"
