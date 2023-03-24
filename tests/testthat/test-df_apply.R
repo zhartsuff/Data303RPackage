4837c22606a960bdfa063dbc8efb7867f1046475
garcie <you@example.com>
2023-03-24 19:46:22 -0400

    add more description

--- tests/testthat/test-df_apply.R
+++ tests/testthat/test-df_apply.R
@@ -6,7 +6,7 @@ test_that("Our function properly turns character vectors in a data frame to uppe
   expect_equal(df_apply(simple_df, toupper, is.character), simple_up)
 })
 
-test_that("We can pass in arguments to .function with ...", {
+test_that("We can use more complex arguments and pass in arguments to .function with ...", {
   expect_equal(df_apply(decimal_data, .fun = round,
                         .filter = function(x) { is.numeric(x) && (mean(x) > 10)}, digits = -1),
                dd_rounded1)
